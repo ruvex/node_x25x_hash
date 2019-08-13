4c3aa3374f72924764bc3c457267ecca7908d53e
Ryxor <team@umbru.io>
2019-08-13 18:19:36 +1200

    Conditionally export bindings

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-module.exports = require('bindings')('nodex25x.node')
+if (require('fs').accessSync) {
+    module.exports = require('bindings')('nodex25x.node');
+} else {
+    console.log('Cannot export bindings (no fs access).');
+}
